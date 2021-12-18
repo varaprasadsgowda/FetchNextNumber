e8acc93e5c972eae5a9286cd119ebb44fc872ccd
varaprasadsgowda <varaprasadsaaa@gmail.com>
2021-12-18 09:26:58 +0530

    added business logic of the problem

--- src/main/java/com/example/FetchNextNumber/controller/Controller.java
+++ src/main/java/com/example/FetchNextNumber/controller/Controller.java
@@ -2,6 +2,8 @@ package com.example.FetchNextNumber.controller;
 
 import com.example.FetchNextNumber.model.returnObject;
 import com.example.FetchNextNumber.model.table;
+import com.example.FetchNextNumber.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,13 +11,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Controller {
 
-    @GetMapping("/FetchNextNumber/{id}")
-    @ResponseBody
-    public returnObject updateTable(@PathVariable int id){
-        returnObject obj = new returnObject("a","b");
-        System.out.println(id);
-        return obj;
+    Service service;
 
+    @Autowired
+    Controller(Service service){
+        this.service = service;
+    }
+
+    @GetMapping("/FetchNextNumber/{id}")
+    public ResponseEntity<returnObject>  updateTable(@PathVariable int id){
+
+        returnObject ans = service.bissnessLogic(id);
+        return new ResponseEntity<>(ans,HttpStatus.OK);
     }
 
 }
--- src/main/java/com/example/FetchNextNumber/service/Service.java
+++ src/main/java/com/example/FetchNextNumber/service/Service.java
@@ -1,7 +1,26 @@
 package com.example.FetchNextNumber.service;
 
+import com.example.FetchNextNumber.model.returnObject;
+import com.example.FetchNextNumber.model.table;
+import com.example.FetchNextNumber.repository.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
 @org.springframework.stereotype.Service
 public class Service {
 
+    Repository repository;
 
+    @Autowired
+    Service(Repository repository){
+        this.repository = repository;
+    }
+
+    public returnObject bissnessLogic(int id){
+        table oldEntry = repository.getById(String.valueOf(id));
+
+
+        return null;
+    }
 }
